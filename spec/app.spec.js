ddff07e2a82c090bbada0e4eedff46a487d8deba
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-03 16:16:29 +0100

    generator-knexpress elements added

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -7,3 +7,22 @@ const supertest = require('supertest');
 
 chai.use(require('chai-sorted'));
 const app = require('../app');
+const connection = require('../db/connection');
+
+const request = supertest(app);
+
+describe('/', () => {
+  // beforeEach(() => connection.seed.run());
+  after(() => connection.destroy());
+
+  describe('/api', () => {
+    it('GET status:200', () => {
+      return request
+        .get('/api')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.ok).to.equal(true);
+        });
+    });
+  });
+});
