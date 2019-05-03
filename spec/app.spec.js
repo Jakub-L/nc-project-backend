f7c7bd6c263df99fad2a697ed37cfa64105e61c4
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-03 15:30:43 +0100

    minimal app setup

---
+++ spec/app.spec.js
@@ -0,0 +1,8 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const { expect } = chai;
+const supertest = require('supertest');
+
+chai.use(require('chai-sorted'));
+const app = require('../app');
