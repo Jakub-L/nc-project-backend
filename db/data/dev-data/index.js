d0b5c6b79b04214ad74255f199167f59a1a74161
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-03 16:01:39 +0100

    db folder started with sample JSON

---
+++ db/data/dev-data/index.js
@@ -0,0 +1,4 @@
+const pins = require('./pins');
+const users = require('./users');
+
+module.exports = { pins, users };
---
+++ db/data/dev-data/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    username: 'Ben',
+    name: 'Ben'
+  },
+  {
+    username: 'Fraser',
+    name: 'Fraser'
+  },
+  {
+    username: 'Jakub',
+    name: 'Jakub'
+  },
+  {
+    username: 'Richard',
+    name: 'Richard'
+  }
+];
