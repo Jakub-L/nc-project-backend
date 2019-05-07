07c28eb343c22074e648e5693c3db0aa05462c35
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-07 09:58:58 +0100

    controllers added

---
+++ controllers/sites-controller.js
@@ -0,0 +1,81 @@
+const {
+  selectAllSites,
+  selectSite,
+  addSite,
+  modifySite,
+  removeSite,
+} = require('../models/sites-models');
+
+function getAllSites(req, res, next) {
+  const acceptQueries = [
+    'site_id',
+    'name',
+    'min_latitude',
+    'max_latitude',
+    'min_longitude',
+    'max_longitude',
+    'min_altitude',
+    'max_altitude',
+  ];
+  if (req.query.every(query => acceptQueries.includes(query))) {
+    selectAllSites(req.query).then(sites => {
+      res.status(200).json({ sites });
+    });
+  } else {
+    next({ status: 400 });
+  }
+}
+
+function getSite(req, res, next) {
+  selectSite(req.params).then(sites => {
+    if (sites.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [site] = sites;
+      res.status(200).json({ site });
+    }
+  });
+}
+
+function postSite(req, res, next) {
+  addSite(req.params, req.body).then(sites => {
+    if (sites.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [site] = sites;
+      res.status(201).json({ site });
+    }
+  });
+}
+
+function patchSite(req, res, next) {
+  modifySite(req.params, req.body)
+    .then(sites => {
+      if (sites.length === 0) {
+        next({ code: 404 });
+      } else {
+        const [site] = sites;
+        res.status(202).json({ site });
+      }
+    })
+    .catch(next);
+}
+
+function deleteSite(req, res, next) {
+  removeSite(req.params).then(sites => {
+    if (sites.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [site] = sites;
+      res.status(204).json({});
+    }
+  });
+}
+
+module.exports = {
+  getAllSites,
+  getSite,
+  postSite,
+  patchSite,
+  deleteSite,
+};
