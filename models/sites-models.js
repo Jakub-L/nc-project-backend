d863faaae61f7a2999892a3cef4707b870bd6f59
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-07 11:24:39 +0100

    models added

---
+++ models/sites-models.js
@@ -0,0 +1,108 @@
+const connection = require('../db/connection');
+
+function selectAllSites(queryObj) {
+  const sortObj = { sort_by: 'name', order: 'asc' };
+  const sortProps = ['sort_by', 'order'];
+  sortProps.forEach(sortProp => {
+    if (queryObj.hasOwnProperty(sortProp)) {
+      sortObj[sortProp] = queryObj[sortProp];
+      delete queryObj[sortProp];
+    }
+  });
+
+  let rawCondition = '0 = 0';
+  for (prop in queryObj) {
+    if (/^max_/.test(prop)) {
+      rawCondition = `${rawCondition} AND sites.${prop.replace(
+        /^max_/,
+        ''
+      )} <= ${queryObj[prop]}`;
+      delete queryObj[prop];
+    } else if (/^min_/.test(prop)) {
+      rawCondition = `${rawCondition} AND sites.${prop.replace(
+        /^min_/,
+        ''
+      )} >= ${queryObj[prop]}`;
+      delete queryObj[prop];
+    }
+  }
+
+  return connection
+    .select(
+      'sites.name AS site',
+      'sites.latitude_min AS latitude_min',
+      'sites.longitude_min AS longitude_min',
+      'sites.altitude_min AS altitude_min',
+      'sites.latitude_max AS latitude_max',
+      'sites.longitude_max AS longitude_max',
+      'sites.altitude_max AS altitude_max'
+    )
+    .count('pins.pin_id AS pin_count', 'users.user_id AS user_count')
+    .from('sites')
+    .leftJoin('pins', 'sites.site_id', '=', 'pins.site_id')
+    .leftJoin('users', 'pins.user_id', '=', 'users.user_id')
+    .groupBy('sites.site_id')
+    .where(queryObj)
+    .whereRaw(rawCondition)
+    .orderBy(sortObj.sort_by, sortObj.order);
+}
+
+function selectSite(paramObj) {
+  for (prop in paramObj) {
+    paramObj[`pins.${prop}`] = paramObj[prop];
+    delete paramObj[prop];
+  }
+  return connection
+    .select(
+      'sites.name AS site',
+      'sites.latitude_min AS latitude_min',
+      'sites.longitude_min AS longitude_min',
+      'sites.altitude_min AS altitude_min',
+      'sites.latitude_max AS latitude_max',
+      'sites.longitude_max AS longitude_max',
+      'sites.altitude_max AS altitude_max'
+    )
+    .count('pins.pin_id AS pin_count', 'users.user_id AS user_count')
+    .from('sites')
+    .leftJoin('pins', 'sites.site_id', '=', 'pins.site_id')
+    .leftJoin('users', 'pins.user_id', '=', 'users.user_id')
+    .groupBy('sites.site_id')
+    .where(paramObj);
+}
+
+function addSite(params, siteReqBody) {
+  const site = {
+    name: siteReqBody.name,
+    latitude_min: siteReqBody.latitude_min,
+    longitude_min: siteReqBody.longitude_min,
+    altitude_min: siteReqBody.altitude_min,
+    latitude_max: siteReqBody.latitude_max,
+    longitude_max: siteReqBody.longitude_max,
+    altitude_max: siteReqBody.altitude_max,
+  };
+  return connection
+    .insert(site)
+    .into('sites')
+    .returning('*');
+}
+
+function modifySite(paramObj, updateObj) {
+  return connection('sites')
+    .where(paramObj)
+    .update(updateObj)
+    .returning('*');
+}
+
+function removeSite(paramObj) {
+  return connection('sites')
+    .where(paramObj)
+    .del();
+}
+
+module.exports = {
+  selectAllSites,
+  selectSite,
+  addSite,
+  modifySite,
+  removeSite,
+};
