a6822fcc0e667f57c23055587fcadb38bf0dbca0
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-07 11:24:39 +0100

    models added

---
+++ models/pins-models.js
@@ -0,0 +1,115 @@
+const connection = require('../db/connection');
+
+function selectAllPins(queryObj) {
+  const sortObj = { sort_by: 'timestamp', order: 'desc' };
+  const sortProps = ['sort_by', 'order'];
+  sortProps.forEach(sortProp => {
+    if (queryObj.hasOwnProperty(sortProp)) {
+      sortObj[sortProp] = queryObj[sortProp];
+      delete queryObj[sortProp];
+    }
+  });
+
+  let rawCondition = '0 = 0';
+  for (prop in queryObj) {
+    if (prop === 'creator') {
+      queryObj['users.name'] = queryObj[prop];
+      delete queryObj[prop];
+    } else if (prop === 'site') {
+      queryObj['sites.name'] = queryObj[prop];
+      delete queryObj[prop];
+    } else if (/^max_/.test(prop)) {
+      rawCondition = `${rawCondition} AND pins.${prop.replace(
+        /^max_/,
+        ''
+      )} <= ${queryObj[prop]}`;
+      delete queryObj[prop];
+    } else if (/^min_/.test(prop)) {
+      rawCondition = `${rawCondition} AND pins.${prop.replace(
+        /^min_/,
+        ''
+      )} >= ${queryObj[prop]}`;
+      delete queryObj[prop];
+    }
+  }
+
+  return connection
+    .select(
+      'pins.photo_url AS url',
+      'pins.note AS note',
+      'pins.timestamp AS timestamp',
+      'pins.latitude AS latitude',
+      'pins.longitude AS longitude',
+      'pins.altitude AS altitude',
+      'users.name AS creator',
+      'users.email AS contact',
+      'sites.name AS site'
+    )
+    .from('pins')
+    .leftJoin('users', 'pins.user_id', '=', 'users.user_id')
+    .leftJoin('sites', 'pins.site_id', '=', 'sites.site_id')
+    .where(queryObj)
+    .whereRaw(rawCondition)
+    .orderBy(sortObj.sort_by, sortObj.order);
+}
+
+function selectPin(paramObj) {
+  for (prop in paramObj) {
+    paramObj[`pins.${prop}`] = paramObj[prop];
+    delete paramObj[prop];
+  }
+  return connection
+    .select(
+      'pins.photo_url AS url',
+      'pins.note AS note',
+      'pins.timestamp AS timestamp',
+      'pins.latitude AS latitude',
+      'pins.longitude AS longitude',
+      'pins.altitude AS altitude',
+      'users.name AS creator',
+      'users.email AS contact',
+      'sites.name AS site'
+    )
+    .from('pins')
+    .leftJoin('users', 'pins.user_id', '=', 'users.user_id')
+    .leftJoin('sites', 'pins.site_id', '=', 'sites.site_id')
+    .where(paramObj);
+}
+
+function addPin(params, pinReqBody) {
+  const pin = {
+    user_id: pinReqBody.user_id,
+    site_id: pinReqBody.site_id,
+    timestamp: pinReqBody.timestamp,
+    latitude: pinReqBody.latitude,
+    longitude: pinReqBody.longitude,
+    altitude: pinReqBody.altitude,
+    photo_url: pinReqBody.photo_url,
+    note: pinReqBody.note
+  };
+  return connection
+    .insert(pin)
+    .into('pins')
+    .returning('*');
+}
+
+function modifyPin(paramObj, updateObj) {
+  return connection('pins')
+    .where(paramObj)
+    .update(updateObj)
+    .returning('*');
+}
+
+function removePin(paramObj) {
+  return connection('pins')
+    .where(paramObj)
+    .del();
+}
+
+module.exports = {
+  selectAllPins,
+  selectPin,
+  addPin,
+  modifyPin,
+  removePin
+};
---
+++ models/users-models.js
@@ -0,0 +1,62 @@
+const connection = require('../db/connection');
+
+function selectAllUsers(queryObj) {
+  const sortObj = { sort_by: 'name', order: 'asc' };
+  const sortProps = ['sort_by', 'order'];
+  sortProps.forEach(sortProp => {
+    if (queryObj.hasOwnProperty(sortProp)) {
+      sortObj[sortProp] = queryObj[sortProp];
+      delete queryObj[sortProp];
+    }
+  });
+
+  return connection
+    .select('users.name AS name', 'users.email AS contact')
+    .from('users')
+    .where(queryObj)
+    .orderBy(sortObj.sort_by, sortObj.order);
+}
+
+function selectUser(paramObj) {
+  for (prop in paramObj) {
+    paramObj[`users.${prop}`] = paramObj[prop];
+    delete paramObj[prop];
+  }
+  return connection
+    .select('users.name AS name', 'users.email AS contact')
+    .from('users')
+    .where(paramObj);
+}
+
+function addUser(params, userReqBody) {
+  const user = {
+    username: userReqBody.username,
+    name: userReqBody.name,
+    email: userReqBody.email,
+  };
+  return connection
+    .insert(user)
+    .into('users')
+    .returning('*');
+}
+
+function modifyUser(paramObj, updateObj) {
+  return connection('users')
+    .where(paramObj)
+    .update(updateObj)
+    .returning('*');
+}
+
+function removeUser(paramObj) {
+  return connection('users')
+    .where(paramObj)
+    .del();
+}
+
+module.exports = {
+  selectAllUsers,
+  selectUser,
+  addUser,
+  modifyUser,
+  removeUser,
+};
