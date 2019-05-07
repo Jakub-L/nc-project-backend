60eebb38bf2a5f7ef244f3f1df067701f33726c2
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-07 09:58:58 +0100

    controllers added

---
+++ controllers/users-controller.js
@@ -0,0 +1,67 @@
+const {
+  selectAllUsers,
+  selectUser,
+  addUser,
+  modifyUser,
+  removeUser,
+} = require('../models/users-models');
+
+function getAllUsers(req, res, next) {
+  selectAllUsers().then(users => {
+      res.status(200).json({ users });
+  });
+}
+
+function getUser(req, res, next) {
+  selectUser(req.params).then(users => {
+    if (users.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [user] = users;
+      res.status(200).json({ user });
+    }
+  });
+}
+
+function postUser(req, res, next) {
+  addUser(req.params, req.body).then(users => {
+    if (users.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [user] = users;
+      res.status(201).json({ user });
+    }
+  });
+}
+
+function patchUser(req, res, next) {
+  modifyUser(req.params, req.body)
+    .then(users => {
+      if (users.length === 0) {
+        next({ code: 404 });
+      } else {
+        const [user] = users;
+        res.status(202).json({ user });
+      }
+    })
+    .catch(next);
+}
+
+function deleteUser(req, res, next) {
+  removeUser(req.params).then(users => {
+    if (users.length === 0) {
+      next({ status: 404 });
+    } else {
+      const [user] = users;
+      res.status(204).json({});
+    }
+  });
+}
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  postUser,
+  patchUser,
+  deleteUser,
+};
