8af8704a0beb9a22b42595124ca5f06c41cc8f0b
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-08 10:31:57 +0100

    POST requests testing and passed

--- models/sites-models.js
+++ models/sites-models.js
@@ -3,7 +3,7 @@ const connection = require('../db/connection');
 function selectAllSites(queryObj) {
   const sortObj = { sort_by: 'sites.site_name', order: 'asc' };
   const sortProps = ['sort_by', 'order'];
-  sortProps.forEach(sortProp => {
+  sortProps.forEach((sortProp) => {
     if (queryObj.hasOwnProperty(sortProp)) {
       sortObj[sortProp] = queryObj[sortProp];
       delete queryObj[sortProp];
@@ -11,21 +11,19 @@ function selectAllSites(queryObj) {
   });
 
   let rawCondition = '0 = 0';
-  Object.keys(queryObj).forEach(prop => {
+  Object.keys(queryObj).forEach((prop) => {
     if (/^max_/.test(prop)) {
-      rawCondition = `${rawCondition} AND sites.${prop.replace(
-        /^max_/,
-        ''
-      )}_max <= ${queryObj[prop]}`;
+      rawCondition = `${rawCondition} AND sites.${prop.replace(/^max_/, '')}_max <= ${
+        queryObj[prop]
+      }`;
       delete queryObj[prop];
     } else if (/^min_/.test(prop)) {
-      rawCondition = `${rawCondition} AND sites.${prop.replace(
-        /^min_/,
-        ''
-      )}_min >= ${queryObj[prop]}`;
+      rawCondition = `${rawCondition} AND sites.${prop.replace(/^min_/, '')}_min >= ${
+        queryObj[prop]
+      }`;
       delete queryObj[prop];
     }
-  })
+  });
 
   return connection
     .select(
@@ -49,10 +47,10 @@ function selectAllSites(queryObj) {
 }
 
 function selectSite(paramObj) {
-  Object.keys(paramObj).forEach(prop => {
+  Object.keys(paramObj).forEach((prop) => {
     paramObj[`pins.${prop}`] = paramObj[prop];
     delete paramObj[prop];
-  })
+  });
   return connection
     .select(
       'sites.site_id AS site_id',
@@ -62,7 +60,7 @@ function selectSite(paramObj) {
       'sites.altitude_min AS altitude_min',
       'sites.latitude_max AS latitude_max',
       'sites.longitude_max AS longitude_max',
-      'sites.altitude_max AS altitude_max'
+      'sites.altitude_max AS altitude_max',
     )
     .count('pins.pin_id AS pin_count', 'users.user_id AS user_count')
     .from('sites')
@@ -74,7 +72,7 @@ function selectSite(paramObj) {
 
 function addSite(siteReqBody) {
   const site = {
-    name: siteReqBody.name,
+    site_name: siteReqBody.site_name,
     latitude_min: siteReqBody.latitude_min,
     longitude_min: siteReqBody.longitude_min,
     altitude_min: siteReqBody.altitude_min,
