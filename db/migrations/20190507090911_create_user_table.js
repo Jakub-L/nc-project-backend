ba85c6731d7fe4c72523f1b9509813b1e470f04c
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-08 11:03:28 +0100

    unique username enforced, pin username query added

--- db/migrations/20190507090911_create_user_table.js
+++ db/migrations/20190507090911_create_user_table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', userTable => {
     userTable.increments('user_id').primary();
-    userTable.string('username');
+    userTable.string('username').unique();
     userTable.string('name');
     userTable.string('email');
     userTable.string('user_photo', 1000);
