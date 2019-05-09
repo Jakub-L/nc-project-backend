2118b899b48e023c7fa21ee5e43d44fc41a09b4f
Jakub P. Lech <47220449+Jakub-L@users.noreply.github.com>
2019-05-09 15:17:08 +0100

    Add password field to migration

--- db/migrations/20190507090911_create_user_table.js
+++ db/migrations/20190507090911_create_user_table.js
@@ -2,6 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('users', (userTable) => {
     userTable.increments('user_id').primary();
     userTable.string('name');
+    userTable.string('password_hash');
     userTable.string('email').unique();
     userTable.string('user_photo', 1000);
   });
