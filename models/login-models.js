d2ffedebf2584a6cc4f0cc8ad8b9830fdae8bcd5
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-13 14:26:38 +0100

    case-insensitive login

--- models/login-models.js
+++ models/login-models.js
@@ -1,16 +1,22 @@
 const bcrypt = require('bcrypt');
 const connection = require('../db/connection');
 
-
 function checkPassword(email, password) {
-  return connection('users')
-    .where({ email })
-    .then(([user]) => {
-      if (!user) return Promise.reject();
-      const { password_hash, ...returnUser } = user;
-      if (!bcrypt.compareSync(password, password_hash)) return Promise.reject();
-      return returnUser;
-    });
+  const sanitizer = /(<|>|"|'|AND|OR|NOT)/i;
+  if (sanitizer.test(email)) {
+    return Promise.reject();
+  }
+  return (
+    connection('users')
+      // .where({ email })
+      .whereRaw(`LOWER(email) = ${email.toLowerCase()}`)
+      .then(([user]) => {
+        if (!user) return Promise.reject();
+        const { password_hash, ...returnUser } = user;
+        if (!bcrypt.compareSync(password, password_hash)) return Promise.reject();
+        return returnUser;
+      })
+  );
 }
 
 module.exports = { checkPassword };
