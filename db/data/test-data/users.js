3fc0f4a3e4878e387fb88a621732b2fdd4f8ad98
bencrick <northcoders15@Northcoders15s-MacBook-Pro.local>
2019-05-15 10:05:55 +0100

    pins re-added, hyphen removed from email addresses

--- db/data/test-data/users.js
+++ db/data/test-data/users.js
@@ -10,7 +10,7 @@ module.exports = [
   {
     // user_id: 2,
     name: 'Fraser',
-    email: 'fraser@site-seeing.com',
+    email: 'fraser@siteseeing.com',
     password_hash: '$2b$10$DRQ9S3h/Ps41jmHtznAUe.CxSS0bMuzaKQzJun5EAcx6aPfOSxxWS',
     user_photo:
       'https://media.licdn.com/dms/image/C5603AQE-JfM9GkvlFw/profile-displayphoto-shrink_800_800/0?e=1563408000&v=beta&t=o1_WZP_piRqdcYNwDjqCjdTfxccl_m1phfP4XL5CZD4',
@@ -18,7 +18,7 @@ module.exports = [
   {
     // user_id: 3,
     name: 'Jakub',
-    email: 'jakub@site-seeing.com',
+    email: 'jakub@siteseeing.com',
     password_hash: '$2b$10$DRQ9S3h/Ps41jmHtznAUe.CxSS0bMuzaKQzJun5EAcx6aPfOSxxWS',
     user_photo:
       'https://media.licdn.com/dms/image/C5603AQEr2XQ1zmJegQ/profile-displayphoto-shrink_100_100/0?e=1563408000&v=beta&t=juxzJFIGItmoe-p_yUDOP6TMVU_5QLc9zJ_akGlZYAw',
@@ -26,7 +26,7 @@ module.exports = [
   {
     // user_id: 4,
     name: 'Richard',
-    email: 'richard@site-seeing.com',
+    email: 'richard@siteseeing.com',
     password_hash: '$2b$10$DRQ9S3h/Ps41jmHtznAUe.CxSS0bMuzaKQzJun5EAcx6aPfOSxxWS',
     user_photo:
       'https://media.licdn.com/dms/image/C4E03AQFbZB-AwzQf5g/profile-displayphoto-shrink_800_800/0?e=1563408000&v=beta&t=By73QL-7JWDzYy8aMyAeENgyyR9HzGkM-KiSiE4OmTk',
@@ -34,7 +34,7 @@ module.exports = [
   {
     // user_id: 5,
     name: 'Ove',
-    email: 'ove@site-seeing.com',
+    email: 'ove@siteseeing.com',
     password_hash: '$2b$10$DRQ9S3h/Ps41jmHtznAUe.CxSS0bMuzaKQzJun5EAcx6aPfOSxxWS',
     user_photo: 'https://upload.wikimedia.org/wikipedia/en/a/ac/Ove_Arup.jpg',
   },
